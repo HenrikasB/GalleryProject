a47c1442e6f907a134adf98c4736db41aaea92ee
HenrikasB <35893609+HenrikasB@users.noreply.github.com>
2019-02-17 23:11:02 +0200

    Initial project add

--- src/main/java/com/gallery/galleryproject/controller/MainController.java
+++ src/main/java/com/gallery/galleryproject/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.gallery.galleryproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     // TODO: create "get" controller that will load main.html page
+    @RequestMapping("")
+    public String loadMainPage(){
+        return "main.html";
+    }
 }
