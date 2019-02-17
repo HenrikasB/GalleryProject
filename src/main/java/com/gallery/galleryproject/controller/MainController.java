e3a4ba986c1dffcdb9b951b79985757b01902a76
HenrikasB <35893609+HenrikasB@users.noreply.github.com>
2019-02-17 23:14:43 +0200

    Updated @RequestMapping method to GET

--- src/main/java/com/gallery/galleryproject/controller/MainController.java
+++ src/main/java/com/gallery/galleryproject/controller/MainController.java
@@ -2,12 +2,13 @@ package com.gallery.galleryproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
     // TODO: create "get" controller that will load main.html page
-    @RequestMapping("")
-    public String loadMainPage(){
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String loadMainPage() {
         return "main.html";
     }
 }
