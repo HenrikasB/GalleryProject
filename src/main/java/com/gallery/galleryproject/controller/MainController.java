c545ce8c98cc202e98a257914dfb64b61257b385
nikola <shu.xuan.shu@gmail.com>
2019-02-17 21:50:19 +0100

    First task - main page. Look TODOs!

---
+++ src/main/java/com/gallery/galleryproject/controller/MainController.java
@@ -0,0 +1,8 @@
+package com.gallery.galleryproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+    // TODO: create "get" controller that will load main.html page
+}
